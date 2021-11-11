4905c7e4247340b8cfb76f3b726f481087dc8987
Jakob Schaffarczyk <jakobs@js-on.de>
2021-11-11 11:48:33 +0100

    fixed all issues; i'm mf genius!

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-from typing import KeysView
-
-
 def xor(out: str, key: str) -> str:
     res = ""
     for a,b in zip(out, key):
@@ -16,6 +13,14 @@ def get_key(box: dict, val: str) -> str:
         if v == val:
             return k
 
+KEYS = [
+    "1010110100",
+    "1001010101",
+    "0101001010",
+    "1101010110",
+    "0001100101"
+][::-1]
+
 boxes = open("sbox.csv", 'r').readlines()
 S1 = boxes[0].strip().split(",")
 S2 = boxes[1].strip().split(",")
@@ -26,24 +31,18 @@ BOX[2] = {k: v for k,v in zip(sorted(S2), S2)}
 ROUNDS = 5
 LENGTH = 5
 
-KEY = "1011010101"
-outp = "0110101001"
+print("Decrypt our fancy shit!")
+outp = input("Input: ")
 bits = outp
 
-for _ in range(ROUNDS):
+for i in range(ROUNDS):
+    KEY = KEYS[i]
     bits_xor = xor(bits, KEY)
-    print("bits_xor:", bits_xor)
-    first = bits[:5]
-    print("first:", first)
-    last = bits[5:]
-    print("last:", last)
+    first = bits_xor[:5]
+    last = bits_xor[5:]
     first_mapped = get_key(BOX[1], first)
-    print("first_mapped:", first_mapped)
     last_mapped = get_key(BOX[2], last)
-    print("last_mapped:", last_mapped)
     bits = first_mapped + last_mapped
-    print("bits:", bits)
-    break
 
 print()
 print("Key:   ", KEY)
--- encrypt.py
+++ encrypt.py
@@ -8,6 +8,13 @@ def xor(out: str, key: str) -> str:
             res += "1"
     return res
 
+KEYS = [
+    "1010110100",
+    "1001010101",
+    "0101001010",
+    "1101010110",
+    "0001100101"
+]
 
 boxes = open("sbox.csv", 'r').readlines()
 S1 = boxes[0].strip().split(",")
@@ -18,33 +25,18 @@ BOX[2] = {k: v for k,v in zip(sorted(S2), S2)}
 ROUNDS = 5
 LENGTH = 5
 
-KEY = "1011010101"
-
-inp = "0101011001"
+print("Encrypt with the best encryption you'll ever see!")
+inp = input("Input: ")
 bits = inp
 
-for _ in range(ROUNDS):
+for i in range(ROUNDS):
+    KEY = KEYS[i]
     first = bits[:5]
-    print("first:", first)
     last = bits[5:]
-    print("last:", last)
     first_mapped = BOX[1][first]
-    print("first_mapped:", first_mapped)
     last_mapped = BOX[2][last]
-    print("last_mapped:", last_mapped)
     output = first_mapped + last_mapped
-    print("output:", output)
     bits = xor(output, KEY)
-    print("bits:  ", bits)
-    break
-    # first = bits[:5]
-    # last = bits[5:]
-    # first_dec = int(first, 2)
-    # last_dec = int(last, 2)
-    # first_mapped = int(S1[first_dec], 2)
-    # last_mapped = int(S2[last_dec], 2)
-    # output = bin(first_mapped)[2:].zfill(5) + bin(last_mapped)[2:].zfill(5)
-    # bits = xor(output, KEY)
 
 print()
 print("Key:   ", KEY)
